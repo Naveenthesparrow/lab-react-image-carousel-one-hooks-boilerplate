7b0820bbc0121c9f95e7d862dc3562bd5f7afd2f
Naveenthesparrow <naveenneevan6380@gmail.com>
2024-01-04 10:01:11 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,63 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./Carousel.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { images } from "../data/CarouselData";
 
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+class Carousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImageIndex: 0,
+    };
+  }
 
-// complete the function below:
-function Carousel() {
+  switchImage = (direction) => {
+    this.setState((prevState) => {
+      let newIndex;
+
+      if (direction === "forward") {
+        newIndex = (prevState.currentImageIndex + 1) % images.length;
+      } else {
+        newIndex =
+          prevState.currentImageIndex === 0
+            ? images.length - 1
+            : prevState.currentImageIndex - 1;
+      }
+
+      return { currentImageIndex: newIndex };
+    });
+  };
+
+  render() {
+    const { currentImageIndex } = this.state;
+
+    return (
+      <div className="carousel-container">
+        <div className="carousel-content">
+          <img
+            src={images[currentImageIndex].img}
+            alt={images[currentImageIndex].title}
+            className="carousel-image"
+          />
+          <div className="carousel-text">
+            <h2>{images[currentImageIndex].title}</h2>
+            <p>{images[currentImageIndex].subtitle}</p>
+          </div>
+        </div>
+        <div className="arrow">
+          <ArrowBackIosIcon
+            className="carousel-arrow-backward"
+            onClick={() => this.switchImage("backward")}
+          />
+          <ArrowForwardIosIcon
+            className="carousel-arrow-forward"
+            onClick={() => this.switchImage("forward")}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
